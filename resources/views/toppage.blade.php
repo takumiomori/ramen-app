e166428b3096ececc4a1a8cf1dc87a29a43a8f86
takumiomori <takumi.omori.nara@gmail.com>
2024-01-28 06:50:46 +0900

    修正

--- resources/views/toppage.blade.php
+++ resources/views/toppage.blade.php
@@ -283,7 +283,7 @@
     <script src="/vendors/is/is.min.js"></script>
     <script src="https://polyfill.io/v3/polyfill.min.js?features=window.scroll"></script>
     <script src="/vendors/fontawesome/all.min.js"></script>
-    <script src="/assets/js/theme.js"></script>
+    <script src="{{ secure_asset('assets/js/theme.js') }}"></script>
 
     <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@200;300;400;600;700;900&amp;display=swap" rel="stylesheet">
 
