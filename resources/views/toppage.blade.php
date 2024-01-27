35de82ff03ea257062db333b88bd91426433e875
takumiomori <takumi.omori.nara@gmail.com>
2024-01-28 07:14:29 +0900

    修正

--- resources/views/toppage.blade.php
+++ resources/views/toppage.blade.php
@@ -278,11 +278,11 @@
     <!-- ===============================================-->
     <!--    JavaScripts-->
     <!-- ===============================================-->
-    <script src="/vendors/@popperjs/popper.min.js"></script>
-    <script src="/vendors/bootstrap/bootstrap.min.js"></script>
-    <script src="/vendors/is/is.min.js"></script>
+    <script src="{{ secure_asset('vendors/@popperjs/popper.min.js') }}"></script>
+    <script src="{{ secure_asset('vendors/bootstrap/bootstrap.min.js') }}"></script>
+    <script src="{{ secure_asset('vendors/is/is.min.js') }}"></script>
     <script src="https://polyfill.io/v3/polyfill.min.js?features=window.scroll"></script>
-    <script src="/vendors/fontawesome/all.min.js"></script>
+    <script src="/{{ secure_asset('vendors/fontawesome/all.min.js') }}"></script>
     <script src="{{ secure_asset('assets/js/theme.js') }}"></script>
 
     <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@200;300;400;600;700;900&amp;display=swap" rel="stylesheet">
