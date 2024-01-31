45a696188f90df46019cf0a2a58010b3f0fea6e1
takumiomori <takumi.omori.nara@gmail.com>
2024-01-31 14:34:51 +0900

    微調整

--- resources/views/toppage.blade.php
+++ resources/views/toppage.blade.php
@@ -125,11 +125,12 @@
         .label_icon{width:30px; margin:auto 10px auto 0;}
         .icon_img{width:100%;}
         .custom-select{border:1.5px solid gainsboro; border-radius: 5px; padding-left: 10px;}
+        .top_catch{text-align: left;}
         
         @media screen and (max-width: 480px) {
           .logo{width:180px;}
           .display-1{font-size:200%;}
-          .top_catch{font-size:80%;}
+          .top_catch{font-size:80%; text-align: center;}
           .top_title{font-size:150%;}
           .text-danger{font-size:40px;}
           .row{flex-flow:column;
